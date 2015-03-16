9ed2ffce7d821c9ea96d31c1dd3dfe22ef5a5c78
jeffrey.sun <jeffrey.sun@autodesk.com>
2015-03-16 14:27:56 +0800

    Add the test code for restart on crash.

--- ubuntu_docker_env/dockerui/readme.md
+++ ubuntu_docker_env/dockerui/readme.md
@@ -1,7 +1,7 @@
 Get started
 ====
 
-DockerUI is a web interface to docker remote API.
+DockerUI is a web interface to docker remote API. You can browser the images, containers in the web UI.
 
 	$ docker run --name=dockerui -d -p 9000:9000 --restart=always --privileged -v /var/run/docker.sock:/var/run/docker.sock dockerui/dockerui
 
--- ubuntu_docker_env/nodejs-raw/server.js
+++ ubuntu_docker_env/nodejs-raw/server.js
@@ -4,6 +4,10 @@ var http = require('http');
 var expressApp = express();
 expressApp.use(express.static(__dirname + ''));
 
+expressApp
+	.get('/', function(req, res, next){res.send("hello world");})
+	.get('/crash', function(){process.exit(1);}); // test the restart on crash.
+
 var listeningPort = 80;
 http.createServer(expressApp).listen(listeningPort)
 
