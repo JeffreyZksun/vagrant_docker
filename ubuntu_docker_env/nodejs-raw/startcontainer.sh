05d35bf9cfee8822de3796a99755e1a763efc493
jeffrey.sun <jeffrey.sun@autodesk.com>
2015-03-22 22:18:23 +0800

    Support the env variable.

--- ubuntu_docker_env/nodejs-raw/startcontainer.sh
+++ ubuntu_docker_env/nodejs-raw/startcontainer.sh
@@ -1,9 +1,15 @@
 #
+# Usage: ./startcontainer [local|alpha|prod]
+# Default value is local.
+# 
+# Logic:
 # 1. check if the old container exists. Stop and delete it if there is.
 # 2. check if the old image exists. Delete it if there is.
 # 3. create new image based on dockerfile
 # 4. start new container based on new image
 #
+
+# change to the folder where is dockerfile is located.
 
 IMAGE=jeffreyzksun/nodejs:v0.10.24
 CONTAINTER=nodejsserver
@@ -16,5 +22,13 @@ echo "Deleting image" ${IMAGE}
 docker rmi ${IMAGE}
 echo "Building image" ${IMAGE}
 docker build -t ${IMAGE} .
-echo "Starting container" ${CONTAINTER}
-docker run --name ${CONTAINTER} --restart="always" -p 8000:80 -v $(pwd):/service/www -d ${IMAGE}
+
+BUILD_ENV="local"
+if [ "$1" == "" ]; then
+  	BUILD_ENV="local"
+else
+	BUILD_ENV=$1
+fi
+
+echo "Starting container" ${CONTAINTER} "with" ${BUILD_ENV} "env"
+docker run --name ${CONTAINTER} --restart="always" --env build=${BUILD_ENV} -p 8000:80 -v $(pwd):/service/www -d ${IMAGE}
